d5250d0d87aa99a381ad6deb86fc2b0f7c255603
Adrien Dulong <adrien.dulong@gmail.com>
2013-01-03 12:15:56 +0100

    Mise en place de Flask Login
    - L'utilisateur se logge, on lui attribut un token qui est passé dans
    "Set-Cookie"
    - Quand l'utilisateur fait une requete il doit faire passer en header
    dans "Cookie" ce qu'il a récupéré de "Set-Cookie" sous cette forme
    'session="....."'

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import (LoginManager, current_user, login_required,
+                            login_user, logout_user, UserMixin, AnonymousUser,
+                            confirm_login, fresh_login_required)
+
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.secret_key = "momentisLifefrom33" 
 db = SQLAlchemy(app)
 
+
+#Flask Login
+login_manager = LoginManager()
+
+#login_manager.anonymous_user = Anonymous
+login_manager.login_view = "login"
+login_manager.login_message = u"Please log in to access this page."
+login_manager.refresh_view = "reauth"
+
+login_manager.setup_app(app)
+
+
+
 import api.views
-    
-    
--- api/models.py
+++ api/models.py
@@ -8,6 +8,28 @@ class User(db.Model):
     age = db.Column(db.Integer)
     pwd = db.Column(db.String(80))
 
+    # Auth token for Flask Login
+    def get_auth_token(self):
+        """
+        Encode a secure token for cookie
+        """
+        data = [str(self.email), self.password]
+        return login_serializer.dumps(data)
+
+
+    # Fonction necessary for Flask Login
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
     def __init__(self, username, email, age, pwd):
         self.username = username
         self.email = email
