3a6e66e569985871b998a37ac7e942a092da9245
Adrien Dulong <adrien.dulong@gmail.com>
2013-03-27 17:29:08 +0100

    ajout de la config pour amazon

--- config.py
+++ config.py
@@ -28,3 +28,13 @@ class ProductionConfig(Config):
 	SQLALCHEMY_DATABASE_URI = 'mysql://'+USER_DB+':'+PASSWORD_DB+'@localhost/'+NAME_DB
 
 	SERVER_NAME = "api.appmoment.fr"
+
+class AmazonConfig(Config):
+	DEBUG = True
+	USER_DB = "appmoment"
+	PASSWORD_DB = "Jar.12626"
+	NAME_DB = "appmoment"
+	#Emplacement de la database
+	SQLALCHEMY_DATABASE_URI = 'mysql://'+USER_DB+':'+PASSWORD_DB+'@apimoment-mysql.c3hcodpxa4oj.eu-west-1.rds.amazonaws.com/'+NAME_DB
+
+	SERVER_NAME = "api.appmoment.fr"
